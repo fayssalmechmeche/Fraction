6bde96356405fed234be11acd8be9599073ee8d8
fayssal <77855537+fayssalmechmeche@users.noreply.github.com>
2021-04-02 16:50:59 +0200

    Update test.java

--- test.java
+++ test.java
@@ -8,12 +8,12 @@ public class test {
 		Fraction fraction = new Fraction();
 		Fraction fraction2 = new Fraction();
 		System.out.println("Fraction 1  : ");
-		System.out.println("Numerateur est egale à : "+fraction.getNumerateur());
-		System.out.println("Denominateur est egale à :"+fraction.getDenominateur());
+		System.out.println("Numerateur est egale Ã  : "+fraction.getNumerateur());
+		System.out.println("Denominateur est egale Ã  :"+fraction.getDenominateur());
 		System.out.println("fraction2:"+fraction.setFraction());
 		System.out.println("Fraction 2  : ");
-		System.out.println("Numerateur est egale à : "+fraction2.getNumerateur2());
-		System.out.println("Denominateur est egale à :"+fraction2.getDenominateur2());
+		System.out.println("Numerateur est egale Ã  : "+fraction2.getNumerateur2());
+		System.out.println("Denominateur est egale Ã  :"+fraction2.getDenominateur2());
 		System.out.println("fraction2:"+fraction.setFraction2());
 		System.out.println("addition"+fraction.setAddtion());
 		System.out.println("soustraction"+fraction.setSoustraction());
@@ -22,6 +22,6 @@ public class test {
 		System.out.println("\n");
 		fraction2.setPGCD2();
 		System.out.println("\n");
-		System.out.println("multiplication"+fraction.setDivision());
+		System.out.println("division"+fraction.setDivision());
 	}
 }
