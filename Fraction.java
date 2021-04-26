255dcb69b3103b62b08f91dc43a7e8c07ca0747f
fayssal <77855537+fayssalmechmeche@users.noreply.github.com>
2021-04-26 18:41:34 +0200

    Add files via upload

--- Fraction.java
+++ Fraction.java
@@ -1,80 +1,63 @@
 package Fraction;
 
-import java.io.OutputStream;
+
 
 public class Fraction{
 
-    private int denominateur=2;
+    private int denominateur; // on definit une variable denominateur
     
-    private int numerateur=1;
-    private int numerateur2=10;
-    
-  
-    
-    
-    
-Fraction() {
-	 
+    private int numerateur; // on definit une variable numerateur
+   
+    public Fraction(int numerateur,int denominateur) { // on definit une variable local pour pouvoir definir une fraction dans le main 
+	 this.numerateur = numerateur;
+	 this.denominateur=denominateur;
     }
-String setFraction() {
-        String resultat = numerateur +"/"+ denominateur;
-        return  resultat;
-    }
-String setFraction2() {
-    String resultat = numerateur2 +"/"+ denominateur;
-    return  resultat;
-}
-public String setAddtion() {
-    String resultat = numerateur + numerateur2 + "/"+ denominateur ;
-    return resultat; 
-}
-public String setSoustraction() {
-    String resultat = numerateur - numerateur2   + "/"+ denominateur;
-    return resultat; 
-}
-public String setMultiplication() {
-    String resultat = numerateur2*numerateur   + "/"+ denominateur*denominateur;
-    return resultat; 
-}
-public String setDivision() {
-	String resultat = numerateur * denominateur +"/"+ denominateur * numerateur2;
-	return resultat;
-}
+    
 
-public void setPGCD() {
-	 
-	 while (numerateur != denominateur) { // on fait 
-	       if(numerateur > denominateur)
-	             numerateur = numerateur - denominateur;
+public void setAddtion(Fraction fraction2) { //  permet d'additionner les fraction 
+	
+    int numerateur = this.numerateur * fraction2.denominateur + this.denominateur * fraction2.numerateur; // on cree une variable local numerateur qui contient le resultat 
+    int denominateur = this.denominateur * fraction2.denominateur; // on cree une variable local denominateur qui contient le resultat
+    Fraction fractionResultat = new Fraction(numerateur,denominateur); // on cree une fraction qui contient les variables local
+    System.out.println(fractionResultat.numerateur+"/"+fractionResultat.denominateur); // on retourne la fraction resultat qui contient les variables
+}
+public void setSoustraction(Fraction fraction2) {
+	int numerateur = this.numerateur*fraction2.denominateur-this.denominateur*fraction2.numerateur; // on cree une variable local numerateur qui contient le resultat 
+    int denominateur = this.denominateur * fraction2.denominateur; // on cree une variable local denominateur qui contient le resultat
+    Fraction fractionResultat = new Fraction(numerateur,denominateur); // on cree une fraction qui contient les variables local
+    System.out.println(fractionResultat.numerateur+"/"+fractionResultat.denominateur); // on retourne la fraction resultat qui contient les variables
+}
+public void setMultiplication(Fraction fraction2) {
+	int numerateur = this.numerateur * fraction2.numerateur; // on cree une variable local numerateur qui contient le resultat 
+    int denominateur = this.denominateur * fraction2.denominateur; // on cree une variable local denominateur qui contient le resultat
+    Fraction fractionResultat = new Fraction(numerateur,denominateur); // on cree une fraction qui contient les variables local
+    System.out.println(fractionResultat.numerateur+"/"+fractionResultat.denominateur); // on retourne la fraction resultat qui contient les variables
+    }
+
+public void setDivision(Fraction fraction2) {
+	int numerateur = this.numerateur * fraction2.denominateur; // on cree une variable local numerateur qui contient le resultat 
+    int denominateur = this.denominateur * fraction2.numerateur; // on cree une variable local denominateur qui contient le resultat
+    Fraction fractionResultat = new Fraction(numerateur,denominateur); // on cree une fraction qui contient les variables local
+    System.out.println(fractionResultat.numerateur+"/"+fractionResultat.denominateur); // on retourne la fraction resultat qui contient les variables
+    }
+
+
+
+public void setPGCD(Fraction fraction2) { 
+	 while (fraction2.numerateur != denominateur) { // on fait une boucle 
+	       if(fraction2.numerateur > denominateur)  // on fait un if qui consiste à tant que le numerateur de la fraction 2 est superieur au denominateur de la fraction 1 
+	             fraction2.numerateur = fraction2.numerateur - denominateur; // donc le numerateur est soustrait par le denominateur
 	       else
-	             denominateur = denominateur - numerateur;
+	             denominateur = denominateur - fraction2.numerateur; // sinon  on fait le contraire
 	     }
-	           
- System.out.printf("PGCD = %d", denominateur);
- }
-public void setPGCD2() {
-	 
-	 while (numerateur2 != denominateur) { // on fait 
-	       if(numerateur2 > denominateur)
-	             numerateur2 = numerateur2 - denominateur;
-	       else
-	             denominateur = denominateur - numerateur2;
-	     }
-	           
-System.out.printf("PGCD = %d", denominateur);
+	 System.out.printf("PGCD = %d", denominateur); // on affiche le pgdcd
 }
 
 public int getNumerateur() {
-    return this.numerateur;
+    return this.numerateur; // on retourne le numerateur
 }
 public int getDenominateur() {
-    return this.denominateur;
-}
-public int getNumerateur2() {
-    return this.numerateur2;
-}
-public int getDenominateur2() {
-    return this.denominateur;
+    return this.denominateur; // on retourne le denominateur 
 }
 
 }
--- test.java
+++ test.java
@@ -5,23 +5,11 @@ package Fraction;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("lol");
-		Fraction fraction = new Fraction();
-		Fraction fraction2 = new Fraction();
-		System.out.println("Fraction 1  : ");
-		System.out.println("Numerateur est egale à : "+fraction.getNumerateur());
-		System.out.println("Denominateur est egale à :"+fraction.getDenominateur());
-		System.out.println("fraction2:"+fraction.setFraction());
-		System.out.println("Fraction 2  : ");
-		System.out.println("Numerateur est egale à : "+fraction2.getNumerateur2());
-		System.out.println("Denominateur est egale à :"+fraction2.getDenominateur2());
-		System.out.println("fraction2:"+fraction.setFraction2());
-		System.out.println("addition"+fraction.setAddtion());
-		System.out.println("soustraction"+fraction.setSoustraction());
-		System.out.println("multiplication"+fraction.setMultiplication());
-		fraction2.setPGCD();
-		System.out.println("\n");
-		fraction2.setPGCD2();
-		System.out.println("\n");
-		System.out.println("division"+fraction.setDivision());
+		Fraction fraction = new Fraction(50,10);
+		Fraction fraction2 = new Fraction(20,5);
+		fraction.setMultiplication(fraction2);
+		fraction.setDivision(fraction2);
+		fraction.setAddtion(fraction2);
+		fraction.setSoustraction(fraction2);
 	}
 }
